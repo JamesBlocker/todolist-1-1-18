1932c7b6f39259e3f565f86f451ac3a6035155a6
Byron <jamesblocker81@gmail.com>
2018-02-01 16:11:59 -0800

    add line-through functionality

--- main.js
+++ main.js
@@ -7,18 +7,22 @@ var $todoVal = "";
 $('.form').submit(function(event) {
     $todoVal = $('#todo').val();
     console.log($todoVal);
-
     var $list = $('.list');
     var $newListItem = $('<p>');
     $newListItem.html('<input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + $todoVal +'</span><i class="glyphicon glyphicon-remove"></i>');
     $list.prepend($newListItem);
-
     $('#todo').val('');
-
-
     event.preventDefault();
 });
 
 
+var $inputCheck = $('input');
+$($inputCheck).on('click', strikeThrough);
+
+function strikeThrough() {
+    var spanTarget = $(this).siblings("span");
+    spanTarget.css("text-decoration", "line-through");
+
+};
 
 
